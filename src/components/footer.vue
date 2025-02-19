4714c26390c334409e2c8d02f20be574afd34417
Max Ward <max.jp.ward@gmail.com>
2025-02-18 17:09:32 -0800

    hide logout button if the user isnt logged in

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,14 +2,21 @@
 	<div class="w-full bg-gray-500 text-white p-4 text-center">
 		<p>&copy; {{ new Date().getFullYear() }} Dogspotter</p>
 		<p class="text-sm text-gray-300">To be used by authorized Dogspotter staff only</p>
-		<button @click="logout" class="text-sm hover:cursor-pointer hover:underline">Logout</button>
+		<button v-if="isLoggedIn" @click="logout" class="text-sm hover:cursor-pointer hover:underline">Logout</button>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import { useAuthStore } from '../store/authStore';
 
 const authStore = useAuthStore();
+
+const isLoggedIn = ref(authStore.isAuthenticated);
+
+watch(() => authStore.isAuthenticated, (val) => {
+	isLoggedIn.value = val;
+});
 
 const logout = () => {
 	authStore.signOut();
